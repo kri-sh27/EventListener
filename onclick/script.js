93a74c1c9f893668063314796e87bac127e728de
kri-sh27 <hogalekrishna2000@gmail.com>
2023-07-20 10:55:35 +0530

    onclick

---
+++ onclick/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("event");
+
+// add a click event listener to the element
+boxElement.addEventListener("click", function () {
+  alert("you just clicked there");
+});
